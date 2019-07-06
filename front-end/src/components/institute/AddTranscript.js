a60dc69cba904a886084e3756b7f15ad1042be9b
Jaideep Singh <SinghJaideepJi@gmail.com>
2019-07-06 19:23:02 -0400

    Made data collection efficient

--- front-end/src/components/institute/AddTranscript.js
+++ front-end/src/components/institute/AddTranscript.js
@@ -4,47 +4,23 @@ class AddTranscript extends Component {
     constructor(props){
         super(props);
         this.state = {
-            transcript_id: '', transcript_date: '', student_id: '',
-            student_name: '', program_name: '', marks_details: ''
+            transcriptID: '', transcriptDate: '', studentID: '',
+            studentName: '', programName: '', marksDetails: ''
         };
 
-        this.handleTranscriptID = this.handleTranscriptID.bind(this);
-        this.handleTranscriptDate = this.handleTranscriptDate.bind(this);
-        this.handleStudentID = this.handleStudentID.bind(this);
-        this.handleStudentName = this.handleStudentName.bind(this);
-        this.handleProgramName = this.handleProgramName.bind(this);
-        this.handleMarksDetails = this.handleMarksDetails.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleTranscriptID(event) {
-        this.setState({transcript_id: event.target.value});
+    handleChange(event) {
+        this.setState({[event.target.id]: event.target.value});
     }
     
-    handleTranscriptDate(event) {
-        this.setState({transcript_date: event.target.value});
-    }
-
-    handleStudentID(event) {
-        this.setState({student_id: event.target.value});
-    }
-
-    handleStudentName(event) {
-        this.setState({student_name: event.target.value});
-    }
-
-    handleProgramName(event) {
-        this.setState({program_name: event.target.value});
-    }
-
-    handleMarksDetails(event) {
-        this.setState({marks_details: event.target.value});
-    }
-
     handleSubmit(event) {
-        alert('Data Submitted: \n TranscriptID: '+ this.state.transcript_id + ' TranscriptDate: '+ this.state.transcript_date + 
-        '\n StudentID: '+ this.state.student_id + ' StudentName: '+ this.state.student_name +
-        '\n ProgramName: '+ this.state.program_name + ' MarksDetails: '+ this.state.marks_details)
+        console.log(this.state);
+        alert('Transcript Added: \n transcriptID: '+ this.state.transcriptID + ' transcriptDate: '+ this.state.transcriptDate + 
+        '\n studentID: '+ this.state.studentID + ' studentName: '+ this.state.studentName +
+        '\n programName: '+ this.state.programName + ' marksDetails: '+ this.state.marksDetails)
         event.preventDefault();
     }
 
@@ -56,33 +32,33 @@ class AddTranscript extends Component {
                 <p> Enter the Transcript Details as per the Standardised Format</p>
                
                 <div className="input-field"> 
-                    <input id="TranscriptID" type="text" className="validate" onChange={this.handleTranscriptID}/>
-                    <label htmlFor="TranscriptID">Transcript ID</label>
+                    <input id="transcriptID" type="text" className="validate" onChange={this.handleChange}/>
+                    <label htmlFor="transcriptID">Transcript ID</label>
                 </div>
 
                 <div className="input-field"> 
-                    <input id="TranscriptDate" type="text" className="validate" onChange={this.handleTranscriptDate}/>
-                    <label htmlFor="TranscriptDate">Transcript Date</label>
+                    <input id="transcriptDate" type="text" className="validate" onChange={this.handleChange}/>
+                    <label htmlFor="transcriptDate">Transcript Date</label>
                 </div>
 
                 <div className="input-field"> 
-                    <input id="StudentID" type="text" className="validate" onChange={this.handleStudentID}/>
-                    <label htmlFor="StudentID">Student ID</label>
+                    <input id="studentID" type="text" className="validate" onChange={this.handleChange}/>
+                    <label htmlFor="studentID">Student ID</label>
                 </div>
 
                 <div className="input-field"> 
-                    <input id="StudentName" type="text" className="validate" onChange={this.handleStudentName}/>
-                    <label htmlFor="StudentName">Student Name</label>
+                    <input id="studentName" type="text" className="validate" onChange={this.handleChange}/>
+                    <label htmlFor="studentName">Student Name</label>
                 </div>
 
                 <div className="input-field"> 
-                    <input id="ProgramName" type="text" className="validate" onChange={this.handleProgramName}/>
-                    <label htmlFor="ProgramName">Program Name</label>
+                    <input id="programName" type="text" className="validate" onChange={this.handleChange}/>
+                    <label htmlFor="programName">Program Name</label>
                 </div>
 
                 <div className="input-field"> 
-                    <input id="MarksDetails" type="text" className="validate" onChange={this.handleMarksDetails}/>
-                    <label htmlFor="MarksDetails">Details of Marks</label>
+                    <input id="marksDetails" type="text" className="validate" onChange={this.handleChange}/>
+                    <label htmlFor="marksDetails">Details of Marks</label>
                 </div>
 
                 <button className="btn waves-effect waves-light" type="submit" name="action">Add Transcript
